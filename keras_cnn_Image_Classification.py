e62cbb2f113faac77cf8205ae4fad905fbf4cf1d
Ankit Vaghela <ankit.vaghela30@gmail.com>
2018-02-25 01:17:46 -0500

    Image classification using Keras

---
+++ keras_cnn_Image_Classification.py
@@ -0,0 +1,192 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 27 20:53:56 2017
+
+@author: Ankit
+"""
+
+# Import libraries
+import os,cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.utils import shuffle
+from sklearn.cross_validation import train_test_split
+
+from keras import backend as K
+K.set_image_dim_ordering('tf')
+
+from keras.utils import np_utils
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.convolutional import Convolution2D, MaxPooling2D
+from keras.optimizers import SGD,RMSprop,adam
+
+#%%
+
+PATH = os.getcwd()
+# Define data path
+data_path = PATH + '/data'
+data_dir_list = os.listdir(data_path)
+
+img_rows=128
+img_cols=128
+num_channel=1
+num_epoch=20
+
+# Define the number of classes
+num_classes = 6
+
+img_data_list=[]
+
+for dataset in data_dir_list:
+	img_list=os.listdir(data_path+'/'+ dataset)
+	print ('Loaded the images of dataset-'+'{}\n'.format(dataset))
+	for img in img_list:
+		input_img=cv2.imread(data_path + '/'+ dataset + '/'+ img )
+		input_img=cv2.cvtColor(input_img, cv2.COLOR_BGR2GRAY)
+		input_img_resize=cv2.resize(input_img,(128,128))
+		img_data_list.append(input_img_resize)
+
+img_data = np.array(img_data_list)
+img_data = img_data.astype('float32')
+img_data /= 255
+print (img_data.shape)
+
+img_data= np.expand_dims(img_data, axis=4) 
+print (img_data.shape)
+	
+#%%
+# Assigning Labels
+
+# Define the number of classes
+num_classes = 6
+
+num_of_samples = img_data.shape[0]
+labels = np.ones((num_of_samples,),dtype='int64')
+
+labels[0:365]=0
+labels[365:785]=0
+labels[785:987]=0
+labels[987:1189]=1
+labels[1189:1391]=2
+labels[1391:1593]=3
+	  
+names = ['bikes','cars','cats','dogs','horses','humans']
+	  
+# convert class labels to on-hot encoding
+Y = np_utils.to_categorical(labels, num_classes)
+
+#Shuffle the dataset
+x,y = shuffle(img_data,Y, random_state=2)
+# Split the dataset
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=2)
+
+#%%
+# Defining the model
+input_shape=img_data[0].shape
+					
+model = Sequential()
+
+model.add(Convolution2D(32, 3,3,border_mode='same',input_shape=input_shape))
+model.add(Activation('relu'))
+model.add(Convolution2D(32, 3, 3))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.5))
+
+model.add(Convolution2D(64, 3, 3))
+model.add(Activation('relu'))
+#model.add(Convolution2D(64, 3, 3))
+#model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.5))
+
+model.add(Flatten())
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes))
+model.add(Activation('softmax'))
+
+#sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+#model.compile(loss='categorical_crossentropy', optimizer=sgd,metrics=["accuracy"])
+model.compile(loss='categorical_crossentropy', optimizer='rmsprop',metrics=["accuracy"])
+
+# Viewing model_configuration
+
+model.summary()
+model.get_config()
+model.layers[0].get_config()
+model.layers[0].input_shape			
+model.layers[0].output_shape			
+model.layers[0].get_weights()
+np.shape(model.layers[0].get_weights()[0])
+model.layers[0].trainable
+
+#%%
+# Training
+hist = model.fit(X_train, y_train, batch_size=16, nb_epoch=num_epoch, verbose=1, validation_data=(X_test, y_test))
+
+
+# visualizing losses and accuracy
+train_loss=hist.history['loss']
+val_loss=hist.history['val_loss']
+train_acc=hist.history['acc']
+val_acc=hist.history['val_acc']
+xc=range(num_epoch)
+
+plt.figure(1,figsize=(7,5))
+plt.plot(xc,train_loss)
+plt.plot(xc,val_loss)
+plt.xlabel('num of Epochs')
+plt.ylabel('loss')
+plt.title('train_loss vs val_loss')
+plt.grid(True)
+plt.legend(['train','val'])
+#print plt.style.available # use bmh, classic,ggplot for big pictures
+plt.style.use(['classic'])
+
+plt.figure(2,figsize=(7,5))
+plt.plot(xc,train_acc)
+plt.plot(xc,val_acc)
+plt.xlabel('num of Epochs')
+plt.ylabel('accuracy')
+plt.title('train_acc vs val_acc')
+plt.grid(True)
+plt.legend(['train','val'],loc=4)
+#print plt.style.available # use bmh, classic,ggplot for big pictures
+plt.style.use(['classic'])
+
+#%%
+
+# Evaluating the model
+
+score = model.evaluate(X_test, y_test, verbose=0)
+print('Test Loss:', score[0])
+print('Test accuracy:', score[1])
+
+test_image = X_test[0:1]
+print (test_image.shape)
+
+print(model.predict(test_image))
+print(model.predict_classes(test_image))
+print(y_test[0:1])
+
+# Testing a new image
+test_image = cv2.imread('data/Humans/rider-8.jpg')
+test_image=cv2.cvtColor(test_image, cv2.COLOR_BGR2GRAY)
+test_image=cv2.resize(test_image,(128,128))
+test_image = np.array(test_image)
+test_image = test_image.astype('float32')
+test_image /= 255
+print (test_image.shape)
+   
+test_image= np.expand_dims(test_image, axis=3) 
+test_image= np.expand_dims(test_image, axis=0)
+print (test_image.shape)
+				
+# Predicting the test image
+print((model.predict(test_image)))
+print(model.predict_classes(test_image))
+
